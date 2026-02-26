408158ae2c1c3c681538d1a8be2b928ee0a9af36
Fernanda <f3castellan@gmail.com>
2026-02-26 19:33:35 -0300

    add 26 02 desafio caixadrive

---
+++ 26 02 caixadrive-concessionaria/AULA4-projeto-professor/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
+import { LoginComponent } from './components/login.component/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PerfilComponent } from './components/perfil.component/perfil.component';
+import { ClienteComponent } from './components/cliente.component/cliente.component';
+import { clienteResolver } from './clientes.resolver';
+export const routes: Routes = [
+
+    // Rota de Login 
+    {path:'login',component: LoginComponent},
+
+    {
+        path:'dashboard',
+        component:LayoutComponent,
+        canActivate:[authGuard], //Protege a Rota pai, ninguem acessa sem estar logado
+        canActivateChild:[authGuard], //Protege toda as rotas filhas automatticamente
+
+        // Rotas Filhas
+    children:[
+        {path:'perfil',
+            component:PerfilComponent
+        },
+        {path:'clientes',
+            component:ClienteComponent,
+            // data passa informações para o guard como pode acessa o admin
+            data:{role:"admin"},
+            // Resolver  só vai carregar a tela quando clienteResolver terminar de buscar na base de dados
+            resolve:{lista:clienteResolver}
+        }
+    ]
+    },
+    // rota padrão
+    {path:'',redirectTo:'login',pathMatch:'full'}
+    
+];
